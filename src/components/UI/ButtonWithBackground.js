ab2ce102c7f7bfc39a49e626ec80e86d0209aa99
Tylyps <j.chmielewski@o2.pl>
2019-03-08 02:28:07 +0100

    Make diffrent button from UI depend of OS

--- src/components/UI/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, TouchableNativeFeedback, View, StyleSheet, Platform } from 'react-native';
 
-const buttonWithBackground = props => (
-  <TouchableOpacity
-    onPress={props.onPress}
-  >
+const buttonWithBackground = props => {
+  const content = (
     <View style={[styles.button, {backgroundColor: props.color}]}>
       <Text style={[styles.text, {backgroundColor: props.textColor}]}>
         {props.children}
       </Text>
-    </View>
-  </TouchableOpacity>
-);
+    </View>);
+
+  if (Platform.OS === 'android') {
+    return (
+      <TouchableNativeFeedback
+        onPress={props.onPress}
+      >
+        {content}
+      </TouchableNativeFeedback>
+    );
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+    >
+      {content}
+    </TouchableOpacity>
+  );
+};
 
 
 const styles = StyleSheet.create({
