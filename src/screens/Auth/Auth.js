5f820db503989b2f922e1e1cd4da5e0e243939a9
Tylyps <j.chmielewski@o2.pl>
2019-03-09 01:29:29 +0100

    Adjusting Styles Dynamically in state

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, ImageBackground } from 'react-native';
+import { View, Button, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
 import backgroundImage from '../../assets/background.jpg';
 
@@ -11,26 +11,69 @@ import MainText from "../../components/UI/MainText";
 
 
 class AuthScreen extends Component {
+  state = {
+    style: {
+      passwordContainer: {
+        flexDirection: Dimensions.get('window').height > 500 ? "column" : "row",
+        justifyContent: "space-between",
+      },
+      passwordWrapper: {
+        width: Dimensions.get('window').height > 500 ? "100%" : "45%",
+      }
+    },
+  };
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", dims => {
+      this.setState({
+        style: {
+          passwordContainer: {
+            flexDirection: Dimensions.get('window').height > 500 ? "column" : "row",
+            justifyContent: "space-between",
+          },
+          passwordWrapper: {
+            width: Dimensions.get('window').height > 500 ? "100%" : "45%",
+          }
+        }
+      })
+
+    });
+  };
 
   loginHandler = () => {
     startMainTabs();
   }
 
   render () {
+    const { style } = this.state;
+    let headingText = null;
+    if (Dimensions.get('window').height > 500) {
+      headingText = (
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+      );
+    };
+
     return (
       <ImageBackground
         source={backgroundImage}
         style={styles.backgroundImage}
       >
         <View style={styles.container}>
-          <MainText>
-            <HeadingText>Please Log In</HeadingText>
-          </MainText>
+          {headingText}
           <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
-            <DefaultInput placeholder="Password" style={styles.input} />
-            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            <View style={style.passwordContainer}>
+              <View style={style.passwordWrapper}>
+                <DefaultInput placeholder="Password" style={styles.input} />
+              </View>
+              <View style={style.passwordWrapper}>
+                <DefaultInput placeholder="Confirm Password" style={styles.input} />
+              </View>
+            </View>
           </View>
           <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
             Submit
