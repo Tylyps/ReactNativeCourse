f866ec41d914348847aea42a1c4360238d28c9d6
Tylyps <j.chmielewski@o2.pl>
2019-03-23 00:54:09 +0100

    End course

--- android/app/src/main/java/com/rncourse/MainActivity.java
+++ android/app/src/main/java/com/rncourse/MainActivity.java
@@ -1,9 +1,38 @@
 package com.rncourse;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+        TextView textView2 = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#521751"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#fa923f"));
+        textView.setText("Awesome Places");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        textView2.setTextColor(Color.parseColor("#fa923f"));
+        textView2.setText("React Native Course");
+        textView2.setGravity(Gravity.CENTER | Gravity.BOTTOM);
+        textView2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+
+        view.addView(textView);
+        view.addView(textView2);
+        return view;
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
